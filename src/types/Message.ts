59050d58bed4a731b87a6939c99690f24e6356d2
Gurpal Singh <gillgurpal699@gmail.com>
2022-04-10 16:58:32 +0530

    create `Message` Type

---
+++ src/types/Message.ts
@@ -0,0 +1,35 @@
+// deno-lint-ignore-file no-explicit-any
+import { ReplyPayload } from "./ReplyPayload.ts";
+export type Message = {
+  tts: boolean;
+  timestamp: string;
+  content: string;
+  mentioned_roles: string[] | [];
+  mentioned_channels: string[] | [];
+  mentioned_users: string[] | [];
+  member: {
+    nick: string | null;
+    roles: string[] | [];
+    joined_at: string;
+    mute: boolean;
+    deaf: boolean;
+    hoisted_role: string | null;
+  };
+  id: string;
+  flags: number;
+  channel_id: string;
+  pinned: boolean;
+  embeds: any[];
+  componenets: any[];
+  author: {
+    username: string;
+    discriminator: string;
+    avatar: string;
+    id: string;
+    public_flags: number;
+  };
+  attachments: any[];
+  guild_id: string;
+  edited_timestamp: string | null;
+  reply: (content: ReplyPayload) => any;
+};
