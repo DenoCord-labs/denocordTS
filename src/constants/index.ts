b421644ba6514a9b22998197c9ee509bdde9db4f
Gurpal Singh <gillgurpal699@gmail.com>
2022-04-10 16:56:30 +0530

    update constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,3 @@
-export const BASE_WS_URL = "wss://gateway.discord.gg/?v=9&encoding=json"
+export const BASE_WS_URL = "wss://gateway.discord.gg/?v=9&encoding=json";
+export const BASE_API_URL = "https://discord.com/api/v9";
+export const BASE_AVATAR_URL = "https://cdn.discordapp.com/avatars";
