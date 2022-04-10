8a2b4a3c7d620ec7e61e28db534a1fd93bc99090
Gurpal Singh <gillgurpal699@gmail.com>
2022-04-10 18:11:34 +0530

    create `Button` component

---
+++ src/structures/Button.ts
@@ -0,0 +1,28 @@
+import { Button as ButtonType, ButtonStyle } from "../types/Button.ts";
+export class Button {
+  button: ButtonType = { type: 2 } as ButtonType;
+  constructor() {}
+  setLabel(text: string) {
+    this.button.label = text;
+    return this;
+  }
+  setStyle(style: keyof typeof ButtonStyle) {
+    this.button.style = ButtonStyle[style];
+    return this;
+  }
+  setUrl(url: string) {
+    this.button.url = url;
+    return this;
+  }
+  setDisabled(disabled: boolean) {
+    this.button.disabled = disabled;
+    return this;
+  }
+  setCustomId(id: string) {
+    this.button.custom_id = id;
+    return this;
+  }
+  get() {
+    return this.button;
+  }
+}
