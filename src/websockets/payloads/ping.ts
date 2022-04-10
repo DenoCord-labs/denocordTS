6fdc4bc213f53ffc7e82578d18de003fc6413ce4
Gurpal Singh <gillgurpal699@gmail.com>
2022-04-10 16:59:05 +0530

    refactor ping function

--- src/websockets/payloads/ping.ts
+++ src/websockets/payloads/ping.ts
@@ -1,10 +1,14 @@
 import { WebSocketClient } from "https://deno.land/x/websocket@v0.1.3/mod.ts";
 
-
-export function sendPingPayload(websocket: WebSocketClient) {
+export function sendPingPayload(
+  websocket: WebSocketClient,
+  heartbeat_interval: number
+) {
   const payload = JSON.stringify({
     op: 1,
     d: null,
   });
-  websocket.send(payload);
+  setInterval(() => {
+    websocket.send(payload);
+  }, heartbeat_interval);
 }
