ed539067b900585518b7677be6c5fca3fd445c82
Gurpal Singh <gillgurpal699@gmail.com>
2022-04-10 18:13:24 +0530

    add new properties to reply payload

--- src/types/ReplyPayload.ts
+++ src/types/ReplyPayload.ts
@@ -1,8 +1,16 @@
 import { Embed } from "./Embed.ts";
+import { MessageActionRow } from "./ActionRow.ts";
 export type ReplyPayload = {
-  content: string;
-  components?: any[];
+  content?: string;
+  components?: MessageActionRow[];
   embeds?: Embed[];
   allowed_mentions?: boolean;
   attachments?: any[];
+  message_reference?: MessageReference;
+};
+
+export type MessageReference = {
+  channel_id: string;
+  message_id: string;
+  guild_id: string;
 };
