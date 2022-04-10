d05a3d5375007f199293bae0059b10e4ffd0ce15
Gurpal Singh <gillgurpal699@gmail.com>
2022-04-10 18:11:46 +0530

    fix `Message` class

--- src/structures/Message.ts
+++ src/structures/Message.ts
@@ -1,4 +1,4 @@
-import { Message as MessageType } from "../types/Message.ts";
+import { MessageWithDelete } from "../types/Message.ts";
 import { ReplyPayload } from "../types/ReplyPayload.ts";
 import { ApiRequest } from "../helpers/request.ts";
 import { BASE_API_URL } from "../constants/index.ts";
@@ -6,19 +6,33 @@ export class Message {
   /**
    * Represents a Message with functions to interact with it
    */
-  constructor(public msg: MessageType, private token: string) {}
+  constructor(public msg: MessageWithDelete, private token: string) {}
   /**
    *
    * @param {ReplyPayload} payload The payload to send
-   * @returns {Promise<MessageType>} The sent message
    */
-  async reply(payload: ReplyPayload): Promise<MessageType> {
+  async reply(payload: ReplyPayload) {
+    if (payload.components && payload.components?.length > 5) {
+      throw new Error("You can only add 5 ActionRows to a Message");
+    }
     const message = await new ApiRequest(
       `${BASE_API_URL}/channels/${this.msg.channel_id}/messages`,
       "POST",
       payload,
       this.token
     ).send();
-    return message.json();
+    const obj: MessageWithDelete = {
+      ...(await message.json()),
+      delete: this.delete,
+    };
+    return new Message(obj, this.token);
+  }
+  async delete() {
+    await new ApiRequest(
+      `${BASE_API_URL}/channels/${this.msg.channel_id}/messages/${this.msg.id}`,
+      "DELETE",
+      {},
+      this.token
+    ).send();
   }
 }
