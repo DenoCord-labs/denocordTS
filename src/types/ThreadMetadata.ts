81a16fe06e05b4d661b95922517a0eefb4be1222
Gurpal Singh <gillgurpal699@gmail.com>
2022-04-11 19:53:52 +0530

    add ThreadMetadata type

---
+++ src/types/ThreadMetadata.ts
@@ -0,0 +1,8 @@
+export type ThreadMetadata = {
+  archived: boolean;
+  auto_archive_duration: number;
+  archive_timestamp: string;
+  locked: boolean;
+  invitable?: boolean;
+  created_time: string;
+};
