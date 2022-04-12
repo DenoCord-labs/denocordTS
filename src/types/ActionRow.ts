0ebc00b31d2884d8d93723aacbbf821a9da60ba0
Gurpal Singh <gillgurpal699@gmail.com>
2022-04-12 16:09:54 +0530

    fix types in `ActionRow.ts`

--- src/types/ActionRow.ts
+++ src/types/ActionRow.ts
@@ -1,7 +1,6 @@
 import { Button } from "./Button.ts";
 import { SelectMenu } from "./SelectMenu.ts";
-import { TextInput } from "./TextInput.ts";
-export type Components = Button | SelectMenu | TextInput;
+export type Components = Button | SelectMenu;
 export type MessageActionRow = {
   type: 1;
   components: Components[];
