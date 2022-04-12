f857500f881e758cea4739d79c5c0bd13b8fee09
MarkosTh09 <markos.theocharis2009@gmail.com>
2022-04-12 18:24:58 +0300

    Types for application commands and gateway op codes

--- src/websockets/payloads/ping.ts
+++ src/websockets/payloads/ping.ts
@@ -1,14 +1,19 @@
 import { WebSocketClient } from "https://deno.land/x/websocket@v0.1.3/mod.ts";
+import { OPCodes } from "../../types/Gateway.ts";
 
 export function sendPingPayload(
   websocket: WebSocketClient,
   heartbeat_interval: number
 ) {
   const payload = JSON.stringify({
-    op: 1,
-    d: null,
+    op: OPCodes.HEARTBEAT,
+    d: null
   });
   setInterval(() => {
+    const start = Date.now();
     websocket.send(payload);
+    websocket.on("message", async (d) => {
+      console.log(`Pong! Ping: ${Date.now() - start}ms`);
+    });
   }, heartbeat_interval);
 }
