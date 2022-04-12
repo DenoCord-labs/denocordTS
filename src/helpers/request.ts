b089e451423edcdc99d2d7c599c98722f6e307bc
Gurpal Singh <gillgurpal699@gmail.com>
2022-04-12 16:09:19 +0530

    remove console log and made token optional `request.ts`

--- src/helpers/request.ts
+++ src/helpers/request.ts
@@ -4,19 +4,18 @@ export class ApiRequest {
   constructor(
     private url: string,
     private method: string,
+    // deno-lint-ignore no-explicit-any
     private body: any,
-    private token: string
-  ) {
-    console.log(JSON.stringify(body));
-  }
+    private token?: string
+  ) {}
   async send() {
+    const headers = new Headers();
+    headers.set("Content-Type", "application/json");
+    if (this.token) headers.set("Authorization", `Bot ${this.token}`);
     const res = await fetch(`${BASE_API_URL}/${this.url}`, {
       method: this.method,
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bot ${this.token}`
-      },
-      body: JSON.stringify(this.body)
+      headers,
+      body: JSON.stringify(this.body),
     });
 
     if (!res.ok)
