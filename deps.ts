56a3a3bb1be1ed0f85c31014bdf2e78228ee6ac8
Gurpal Singh <gillgurpal699@gmail.com>
2022-04-19 17:16:42 +0530

    remove all dependencies except 2

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/x/websocket@v0.1.3/mod.ts";
 export * from "https://deno.land/x/eventemitter@1.2.1/mod.ts";
+export * from "https://deno.land/x/discord_api_types@0.31.1/v10.ts";
