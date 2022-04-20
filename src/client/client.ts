063f23a3f550c9283a3b3b3f395d852f841886be
Gurpal Singh <gillgurpal699@gmail.com>
2022-04-20 14:45:12 +0530

    add `setPresence` to `Client` class

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,7 +1,18 @@
 import { Base } from "./base.ts";
 import { ClientOptions } from "../types/mod.ts";
+import { GatewayPresenceUpdateData, GatewayOpcodes } from "../../deps.ts";
 export class Client extends Base {
   constructor(protected options: ClientOptions) {
     super(options);
+  }
+  setPresence(presence: GatewayPresenceUpdateData) {
+    this.websocket.send(
+      JSON.stringify({
+        op: GatewayOpcodes.PresenceUpdate,
+        d: {
+          ...presence,
+        },
+      })
+    );
   }
 }
