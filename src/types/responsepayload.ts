abbf8adecaeeb418e782c20c221f6ee892a0c10d
Gurpal Singh <gillgurpal699@gmail.com>
2022-04-21 14:58:54 +0530

    create types for Response to get from the user

---
+++ src/types/responsepayload.ts
@@ -0,0 +1,34 @@
+import {
+	APIActionRowComponent,
+	APIEmbed,
+	APIMessageActionRowComponent,
+} from "../../deps.ts";
+
+export type ResponsePayload = {
+	content?: string;
+	components?: APIActionRowComponent<APIMessageActionRowComponent>[];
+	embeds?: APIEmbed[];
+	allowed_mentions?: boolean;
+	attachments?: MessageAttachment[];
+	message_reference?: MessageReference;
+	flags?: number;
+};
+
+export type ReplyPayload = ResponsePayload & {
+	ephemeral?: boolean;
+	suppress_embeds?: boolean;
+};
+
+export type MessageReference = {
+	channel_id: string;
+	message_id: string;
+	guild_id: string;
+};
+
+export type MessageAttachment = {
+	id: string;
+	filename: string;
+	size: number;
+	ephemeral?: boolean;
+	url: string;
+};
