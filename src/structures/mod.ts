98e57c2197740b1769ce7094c8190d64ed5de223
Gurpal Singh <gillgurpal699@gmail.com>
2022-04-23 15:37:11 +0530

    add `Embed` export in `structures/mod.ts`

--- src/structures/mod.ts
+++ src/structures/mod.ts
@@ -1,3 +1,4 @@
 export { ClientMessage, Message } from "./messages/mod.ts";
 export { ActionRow } from "./ActionRow/mod.ts";
 export { Button, SelectMenu } from "./components/mod.ts";
+export { Embed } from "./embeds/mod.ts";
