cfb6b3e8abe54139da5d6c0954c3585ea126640a
Gurpal Singh <gillgurpal699@gmail.com>
2022-04-23 15:35:55 +0530

    add export for `Embeds`

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
 export * from "./src/client/client.ts";
 export { ActivityType, PresenceUpdateStatus } from "./src/types/mod.ts";
-export { ActionRow, Button, SelectMenu } from "./src/structures/mod.ts";
+export { ActionRow, Button, SelectMenu, Embed } from "./src/structures/mod.ts";
 export { parseEmojiForComponents } from "./src/utils/mod.ts";
