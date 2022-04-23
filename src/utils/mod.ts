e6de8ed51279a53c682ca1882dfc93c4ae68338e
Gurpal Singh <gillgurpal699@gmail.com>
2022-04-23 15:38:09 +0530

    create `ColorResolvable` function

--- src/utils/mod.ts
+++ src/utils/mod.ts
@@ -1,3 +1,12 @@
+import { Colors } from "../constants/mod.ts";
+
+export type HexColorString = `#${string}`;
+export type ColorResolvable =
+  | keyof typeof Colors
+  | "RANDOM"
+  | readonly [red: number, green: number, blue: number]
+  | number
+  | HexColorString;
 export function parseEmoji(text: any) {
   if (text.includes("%")) text = decodeURIComponent(text);
   if (text.includes("<a:") || text.includes("<:")) {
@@ -13,4 +22,20 @@ export function parseEmojiForComponents(text: any) {
     return { animated: false, name: text, id: undefined };
   const match = text.match(/<?(?:(a):)?(\w{2,32}):(\d{17,19})?>?/);
   return match && { animated: Boolean(match[1]), name: match[2], id: match[3] };
+}
+
+export function resolveColor(color: ColorResolvable) {
+  if (typeof color === "string") {
+    if (color === "RANDOM") return Math.floor(Math.random() * (0xffffff + 1));
+    if (color === "DEFAULT") return 0;
+    //@ts-ignore - TS doesn't know about Colors
+    color = Colors[color] ?? parseInt(color.replace("#", ""), 16);
+  } else if (Array.isArray(color)) {
+    color = (color[0] << 16) + (color[1] << 8) + color[2];
+  }
+
+  if (color < 0 || color > 0xffffff) throw new RangeError("COLOR_RANGE");
+  else if (Number.isNaN(color)) throw new TypeError("COLOR_CONVERT");
+
+  return color;
 }
