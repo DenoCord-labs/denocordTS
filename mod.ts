e4e11aa657036549526381f69033bce5a3b37bf0
Gurpal Singh <gillgurpal699@gmail.com>
2022-04-25 17:27:08 +0530

    create exports for components collectors

--- mod.ts
+++ mod.ts
@@ -1,4 +1,12 @@
 export * from "./src/client/client.ts";
 export { ActivityType, PresenceUpdateStatus } from "./src/types/mod.ts";
-export { ActionRow, Button, SelectMenu, Embed } from "./src/structures/mod.ts";
+export type { APIMessageComponentSelectMenuInteraction } from "./src/types/mod.ts";
+export {
+  ActionRow,
+  Button,
+  SelectMenu,
+  Embed,
+  ComponentCollector,
+} from "./src/structures/mod.ts";
 export { parseEmojiForComponents } from "./src/utils/mod.ts";
+export * from "./src/helpers/mod.ts";
