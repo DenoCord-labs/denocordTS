978dd0b9119b30309f4afe36a7d8c2822612637a
Gurpal Singh <gillgurpal699@gmail.com>
2022-04-25 17:28:39 +0530

    add export for component collector

--- src/structures/mod.ts
+++ src/structures/mod.ts
@@ -2,3 +2,4 @@ export { ClientMessage, Message } from "./messages/mod.ts";
 export { ActionRow } from "./ActionRow/mod.ts";
 export { Button, SelectMenu } from "./components/mod.ts";
 export { Embed } from "./embeds/mod.ts";
+export { ComponentCollector } from "./collectors/mod.ts";
