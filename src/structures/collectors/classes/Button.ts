34b2b83006c7fadf1f4f5ce379ba49f527f64520
Gurpal Singh <gillgurpal699@gmail.com>
2022-04-25 17:29:51 +0530

    create `Button.ts` component class(collector)

---
+++ src/structures/collectors/classes/Button.ts
@@ -0,0 +1,12 @@
+import { BaseComponent } from "./base.ts";
+import { Client } from "../../../client/client.ts";
+import { APIMessageComponentButtonInteraction } from "../../../types/mod.ts";
+export class ButtonInteraction extends BaseComponent {
+  constructor(
+    client: Client,
+    channelId: string,
+    d: APIMessageComponentButtonInteraction
+  ) {
+    super(client, channelId, d);
+  }
+}
