bb9d308a288750c35c9ee7347e675e25183ec3a8
Gurpal Singh <gillgurpal699@gmail.com>
2022-05-04 15:39:58 +0530

    switch from `EventEmitter` to `EventTarget` for collectors

--- src/structures/collectors/collector.ts
+++ src/structures/collectors/collector.ts
@@ -1,10 +1,12 @@
-import { EventEmitter } from "../../../deps.ts";
-import { CollectorEvents } from "../../types/collector.ts";
 import { Client } from "../../client/client.ts";
 import { ButtonInteraction } from "./classes/Button.ts";
-import { SlashMenuInteraction as SelectMenuInteraction } from "./classes/SelectMenu.ts";
-export class ComponentCollector extends EventEmitter<CollectorEvents> {
-  constructor(private client: Client, private channelId: string) {
+import { SelectMenuInteraction as SelectMenuInteraction } from "./classes/SelectMenu.ts";
+export class ComponentCollector extends EventTarget {
+  constructor(
+    private client: Client,
+    private channelId: string,
+    disposeInterval?: number
+  ) {
     super();
     this.client.on("InteractionCreate", (e) => {
       if (
@@ -12,20 +14,42 @@ export class ComponentCollector extends EventEmitter<CollectorEvents> {
         e.channel_id == this.channelId &&
         e.data.component_type == 2
       ) {
-        this.emit(
-          "buttonInteraction",
-          new ButtonInteraction(this.client, this.channelId, e),
+        super.dispatchEvent(
+          new CustomEvent("buttonInteraction", {
+            detail: new ButtonInteraction(this.client, this.channelId, e),
+          })
         );
       } else if (
         e.type == 3 &&
         e.channel_id == this.channelId &&
         e.data.component_type == 3
       ) {
-        this.emit(
-          "selectMenuInteraction",
-          new SelectMenuInteraction(this.client, this.channelId, e),
+        super.dispatchEvent(
+          new CustomEvent("selectMenuInteraction", {
+            detail: new SelectMenuInteraction(this.client, this.channelId, e),
+          })
         );
       }
     });
+    if (disposeInterval) {
+      setTimeout(() => {
+        this.dispose();
+      }, disposeInterval);
+    }
+    super.removeEventListener("buttonInteraction", () => {});
+  }
+  dispose() {
+    super.removeEventListener("buttonInteraction", () => {});
+
+    super.removeEventListener("selectMenuInteraction", () => {});
+  }
+  waitForInteraction({
+    type,
+    listener,
+  }: {
+    type: "buttonInteraction" | "selectMenuInteraction";
+    listener: (e: any) => any;
+  }) {
+    super.addEventListener(type, listener);
   }
 }
