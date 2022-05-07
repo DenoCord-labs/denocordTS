00e38192412dbc9ee0b6b9646266c297f7b053fe
Gurpal Singh <gillgurpal699@gmail.com>
2022-05-07 16:37:53 +0530

    move `discord-api-types` to `deps.ts`

--- deps.ts
+++ deps.ts
@@ -1,2 +1,4 @@
 export * from "https://deno.land/x/eventemitter@1.2.1/mod.ts";
 export { camelize } from "https://deno.land/x/camelize@2.0.0/mod.ts";
+export type { Camelize } from "https://deno.land/x/camelize@2.0.0/mod.ts";
+export * from "https://deno.land/x/discord_api_types/v10.ts";
