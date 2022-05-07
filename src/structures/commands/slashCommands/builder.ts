2be9c0b7c00bca5c24913580c15b42ef7ea27d33
Gurpal Singh <gillgurpal699@gmail.com>
2022-05-07 16:40:29 +0530

    add new functions to add Sub commands and groups

--- src/structures/commands/slashCommands/builder.ts
+++ src/structures/commands/slashCommands/builder.ts
@@ -1,93 +1,25 @@
-import { CommandType } from "../../../types/commands/mod.ts";
-import {
-	AttachmentOptions,
-	BooleanOption,
-	ChannelOptions,
-	IntegerOption,
-	MentionableOptions,
-	NumberOptions,
-	RoleOption,
-	StringOption,
-	UserOption,
-} from "./options/mod.ts";
-export class SlashCommand {
-	name = "";
-	name_localizations = undefined;
-	description = "";
-	description_localizations = undefined;
-	options: any[] = [];
-	type = CommandType.ChatInput;
-	dm_permission: undefined | boolean = undefined;
-	constructor() {}
-	setName(name: string) {
-		this.name = name;
+import { SubCommand, SubCommandGroup } from "./options/mod.ts";
+import { BaseApplicationCommand } from "./base.ts";
+import { ApplicationCommandOptionType } from "../../../types/mod.ts";
+export class SlashCommand extends BaseApplicationCommand {
+	constructor() {
+		super();
+	}
+	addSubCommand(callback: (e: SubCommand) => SubCommand): this {
+		this.options.push({
+			...callback(new SubCommand()).toJSON(),
+			type: ApplicationCommandOptionType.Subcommand,
+		});
 		return this;
 	}
-	setDescription(description: string) {
-		if (description.length > 100) {
-			throw new Error("Description must be less than 100 characters");
-		}
-		this.description = description;
-		return this;
-	}
-
-	disableInDm() {
-		this.dm_permission = false;
-		return this;
-	}
-	addStringOption(callback: (e: StringOption) => StringOption): this {
-		this.options.push(callback(new StringOption()).toJSON());
-		return this;
-	}
-	addIntegerOption(callback: (e: IntegerOption) => IntegerOption): this {
-		this.options.push(callback(new IntegerOption()).toJSON());
-		return this;
-	}
-	addBooleanOption(callback: (e: BooleanOption) => BooleanOption): this {
-		this.options.push(callback(new BooleanOption()).toJSON());
-		return this;
-	}
-	addUserOption(callback: (e: UserOption) => UserOption): this {
-		this.options.push(callback(new UserOption()).toJSON());
-		return this;
-	}
-	addRoleOption(callback: (e: RoleOption) => RoleOption): this {
-		this.options.push(callback(new RoleOption()).toJSON());
-		return this;
-	}
-	addMentionableOption(
-		callback: (e: MentionableOptions) => MentionableOptions,
+	addSubCommandGroup(
+		callback: (e: SubCommandGroup) => SubCommandGroup
 	): this {
-		this.options.push(callback(new MentionableOptions()).toJSON());
+		this.options.push({
+			...callback(new SubCommandGroup()).ToJSON(),
+			type: ApplicationCommandOptionType.SubcommandGroup,
+			options: callback(new SubCommandGroup()).ToJSON().options,
+		});
 		return this;
-	}
-	addNumberOption(callback: (e: NumberOptions) => NumberOptions): this {
-		this.options.push(callback(new NumberOptions()).toJSON());
-		return this;
-	}
-	addAttachmentOption(
-		callback: (e: AttachmentOptions) => AttachmentOptions,
-	): this {
-		this.options.push(callback(new AttachmentOptions()).toJSON());
-		return this;
-	}
-	addChannelOption(callback: (e: ChannelOptions) => ChannelOptions): this {
-		this.options.push(callback(new ChannelOptions()).toJSON());
-		return this;
-	}
-	toJSON() {
-		this.options.sort((a, b) => {
-			if (a.required && !b.required) return -1;
-			if (!a.required && b.required) return 1;
-			return 0;
-		}); // move required options to the top
-		return {
-			name: this.name,
-			name_localizations: this.name_localizations,
-			description: this.description,
-			description_localizations: this.description_localizations,
-			options: this.options,
-			dm_permission: this.dm_permission,
-		};
 	}
 }
