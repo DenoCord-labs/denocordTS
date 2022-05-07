09f00360f66c96673cc80f94e735554dfad66341
Gurpal Singh <gillgurpal699@gmail.com>
2022-05-07 16:42:00 +0530

    create new `CommandInteraction` type

---
+++ src/types/commandInteraction.ts
@@ -0,0 +1,18 @@
+import { Camelize } from "../../deps.ts";
+import { GatewayInteractionCreateDispatchData } from "./mod.ts";
+import type { ApplicationCommandInteraction } from "../structures/interaction/commands/applicationCommand.ts";
+export type CommandInteraction =
+	Camelize<GatewayInteractionCreateDispatchData> & {
+		reply: ApplicationCommandInteraction["reply"];
+		deferReply: ApplicationCommandInteraction["deferReply"];
+		editReply: ApplicationCommandInteraction["editReply"];
+		deleteReply: ApplicationCommandInteraction["deleteReply"];
+		editFollowUp: ApplicationCommandInteraction["editFollowUp"];
+		deleteFollowUp: ApplicationCommandInteraction["deleteFollowUp"];
+		followUp: ApplicationCommandInteraction["followUp"];
+		fetchFollowUp: ApplicationCommandInteraction["fetchFollowUp"];
+		isCommand: boolean;
+		isComponentInteraction: boolean;
+		isAutoComplete: boolean;
+		isModalSubmit: boolean;
+	};
