4a327af2109d95673ae4432cd5b9b6d93f296bac
Gurpal Singh <gillgurpal699@gmail.com>
2022-05-07 16:42:27 +0530

    move `discord-api-types` to `deps.ts`

--- src/types/mod.ts
+++ src/types/mod.ts
@@ -3,4 +3,4 @@ export * from "./clientoptions.ts";
 export * from "./gateway.ts";
 export * from "./cache.ts";
 export * from "./client.ts";
-export * from "https://deno.land/x/discord_api_types/v10.ts";
+export * from "../../deps.ts";
