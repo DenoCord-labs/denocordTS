5926e2b9f9af83dbf05d0f4e51dd9c2951d89a52
Gurpal Singh <gillgurpal699@gmail.com>
2022-05-09 12:45:40 +0530

    bug fix in choices length

--- src/structures/commands/slashCommands/options/base.ts
+++ src/structures/commands/slashCommands/options/base.ts
@@ -15,20 +15,19 @@ export class BaseCommandOption {
 		choices?: Array<any>;
 	}) {
 		if (
-			!new RegExp(/^[-_\p{L}\p{N}\p{sc=Devanagari}\p{sc=Thai}]{1,32}$/u)
-				.test(
-					name,
-				)
+			!new RegExp(
+				/^[-_\p{L}\p{N}\p{sc=Devanagari}\p{sc=Thai}]{1,32}$/u
+			).test(name)
 		) {
 			throw new Error(
-				"Name must be between 1 and 32 characters and can only contain letters, numbers, underscores, and dashes.",
+				"Name must be between 1 and 32 characters and can only contain letters, numbers, underscores, and dashes."
 			);
 		}
 		if (description.length > 100) {
 			throw new Error("Description must be less than 100 characters");
 		}
-		if (autocomplete && choices) {
-			throw new Error("Autocomplete and choices are mutually exclusive");
+		if (choices && choices.length > 0 && autocomplete) {
+			throw new Error("AutoComplete and Choices are mutually exclusive.");
 		}
 	}
 }
