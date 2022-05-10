b7d77148739170460ba26b7ca15fb4ffd8705f2d
Gurpal Singh <gillgurpal699@gmail.com>
2022-05-10 16:34:12 +0530

    add export for collection

--- deps.ts
+++ deps.ts
@@ -1,4 +1,5 @@
 export * from "https://deno.land/x/eventemitter@1.2.1/mod.ts";
 export { camelize } from "https://deno.land/x/camelize@2.0.0/mod.ts";
 export type { Camelize } from "https://deno.land/x/camelize@2.0.0/mod.ts";
-export * from "https://deno.land/x/discord_api_types/v10.ts";
+export * from "https://deno.land/x/discord_api_types@0.31.1/v10.ts";
+export * from "https://deno.land/x/collection@v1.0.1/mod.ts";
