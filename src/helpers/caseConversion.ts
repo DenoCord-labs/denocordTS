6c2f78fd2da3271d8c2ddc4c52eddccc1fa1c98e
Gurpal Singh <gillgurpal699@gmail.com>
2022-05-16 15:58:48 +0530

    create `camelToSnakeCase` function

--- src/helpers/caseConversion.ts
+++ src/helpers/caseConversion.ts
@@ -1,5 +1,9 @@
 export function toCamelCase(str: string) {
-  return str.replace(/([-_][a-z])/g, (group) => {
-    return group.toUpperCase().replace("-", "").replace("_", "");
-  });
+	return str.replace(/([-_][a-z])/g, (group) => {
+		return group.toUpperCase().replace("-", "").replace("_", "");
+	});
+}
+
+export function camelToSnakeCase(str: string) {
+	return str.replace(/([a-z])([A-Z])/g, "$1_$2").toLowerCase();
 }
