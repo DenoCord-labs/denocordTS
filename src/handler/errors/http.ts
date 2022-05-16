5363f361bc7857cad5340cb719d94bf6711f2205
Gurpal Singh <gillgurpal699@gmail.com>
2022-05-16 15:58:35 +0530

    add handler for http errors

---
+++ src/handler/errors/http.ts
@@ -0,0 +1,10 @@
+// Used to handle the Http Error Events
+import { DiscordApiError } from "../../errors/mod.ts";
+
+export class HttpError {
+	constructor(error: any) {
+		throw new DiscordApiError(
+			`[Http Error] StatusCode:${error.statusCode} ApiCode:${error.code} Message:${error.message}`
+		);
+	}
+}
