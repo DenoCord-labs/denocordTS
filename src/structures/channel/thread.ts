da4ac2b7a92932e0788b91d98b18f55b5ee480af
Gurpal Singh <gillgurpal699@gmail.com>
2022-05-17 16:31:22 +0530

    new `ThreadChannel` class

---
+++ src/structures/channel/thread.ts
@@ -0,0 +1,7 @@
+import { BaseChannel } from "./base.ts";
+import { Base } from "../../client/base.ts";
+export class ThreadChannel extends BaseChannel {
+	constructor(d: any, client: Base) {
+		super(d, client);
+	}
+}
