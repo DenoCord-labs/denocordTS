66c78a994a1b87c210190f87e15ff8d210bd8d14
Gurpal Singh <gillgurpal699@gmail.com>
2022-05-19 14:45:19 +0530

    create new rest client and delete older one

---
+++ src/http/rest.ts
@@ -0,0 +1,136 @@
+import { Collection } from "../../deps.ts";
+import { BaseRestApiUrl } from "../constants/mod.ts";
+import { HttpError } from "../handler/errors/http.ts";
+
+export class RestClient extends Collection<
+	string,
+	Record<string, number | Record<string, string | null>>
+> {
+	constructor() {
+		super();
+	}
+	async request(
+		href: string,
+		method: "GET" | "POST" | "PATCH" | "DELETE" | "PUT",
+		body: unknown = {},
+		headers?: HeadersInit,
+		strignifyBody?: boolean
+	) {
+		const url = href.includes("http") ? href : `${BaseRestApiUrl}${href}`;
+		const needsQueue = this.checks(url);
+		if (!window.token)
+			throw new Error("Token Not Provided to an api request!");
+		if (needsQueue.queue === true) {
+			return (await this.createQueue({
+				href,
+				method,
+				body,
+				strignifyBody,
+				headers,
+				time: needsQueue.time || 5,
+			})) as Response;
+		}
+		const res = await fetch(`${url}`, {
+			headers: {
+				"Content-Type": `application/json${
+					strignifyBody ? "" : ";charset=utf-8"
+				} `,
+				...headers,
+				Authorization: `Bot ${window.token!}`,
+			},
+			method,
+			body:
+				method == "GET"
+					? undefined
+					: ((!strignifyBody
+							? JSON.stringify(body)
+							: body) as BodyInit),
+		});
+		if (!res.ok) {
+			new HttpError(await res.json());
+		}
+		this.addUrlToCollection(url, res.headers);
+		return res;
+	}
+	/**
+	 * Add url to collection with its ratelimit data
+	 */
+	private addUrlToCollection(url: string, headers: Headers) {
+		super.set(url, {
+			timestamp: Date.now(),
+			ratelimit: {
+				bucket: headers.get("x-ratelimit-bucket"),
+				limit: headers.get("x-ratelimit-limit"),
+				remaining: headers.get("x-ratelimit-remaining"),
+				reset: headers.get("x-ratelimit-reset"),
+				resetAfter: headers.get("x-ratelimit-reset-after"),
+			},
+		});
+	}
+	private checks(url: string) {
+		const data = super.get(url);
+		if (data === undefined) {
+			return {
+				queue: false,
+			};
+		}
+		if (parseInt((data.ratelimit as Record<string, string>).remaining) > 3)
+			return { queue: false };
+		if (
+			Date.now() / 1000 >
+			parseFloat(
+				(data.ratelimit as Record<string, string | null>).reset ||
+					String((data.timestamp as number) + 5)
+			)
+		)
+			return {
+				queue: false,
+			};
+		if (
+			(Date.now() - (data.timestamp as number)) / 1000 >
+			parseInt(
+				(data.ratelimit as Record<string, string | null>).resetAfter!
+			)
+		) {
+			return {
+				queue: false,
+			};
+		}
+		return {
+			queue: true,
+			time:
+				parseInt(
+					(data.ratelimit as Record<string, string | null>)
+						.resetAfter || "5"
+				) || 3,
+		};
+	}
+	private createQueue({
+		time = 5,
+		body = {},
+		href,
+		method,
+		headers,
+		strignifyBody,
+	}: {
+		time?: number;
+		href: string;
+		method: "GET" | "POST" | "PATCH" | "DELETE" | "PUT";
+		body: any;
+		headers?: HeadersInit;
+		strignifyBody?: boolean;
+	}) {
+		return new Promise((resolve) => {
+			setTimeout(async () => {
+				const res = await this.request(
+					href,
+					method,
+					body,
+					headers,
+					strignifyBody
+				);
+				resolve(res);
+			}, time * 1000);
+		});
+	}
+}
