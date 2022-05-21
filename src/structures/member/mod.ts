5c711063e657e23bf8a0e43ce3c894b81d3fd937
Gurpal Singh <gillgurpal699@gmail.com>
2022-05-21 15:31:05 +0530

    fix casing and add new functions on `GuildMember` class

--- src/structures/member/mod.ts
+++ src/structures/member/mod.ts
@@ -1,43 +1,48 @@
 import { APIGuildMember, PermissionFlagsBits } from "../../types/mod.ts";
 import { Base } from "../../client/base.ts";
 import { RestClient } from "../../http/rest.ts";
-export class GuildMember implements APIGuildMember {
+import { DiscordSnowflake } from '../../../deps.ts'
+import { User } from '../mod.ts'
+import { endpoints } from '../../constants/endpoints/mod.ts'
+export class GuildMember {
 	private rest = new RestClient();
 	avatar: APIGuildMember["avatar"];
-	communication_disabled_until?: APIGuildMember["communication_disabled_until"];
+	communicationDisabledUntil?: APIGuildMember["communication_disabled_until"];
 	deaf: APIGuildMember["deaf"];
-	joined_at: APIGuildMember["joined_at"];
+	joinedAt: APIGuildMember["joined_at"];
 	mute: APIGuildMember["mute"];
 	nick?: APIGuildMember["nick"];
 	pending: APIGuildMember["pending"];
-	premium_since?: APIGuildMember["premium_since"];
+	premiumSince?: APIGuildMember["premium_since"];
 	roles: APIGuildMember["roles"];
-	user: APIGuildMember["user"];
+	user?: User;
 	permission: bigint;
+	createdAt: number
 	constructor(
 		private d: any,
 		private client: Base,
 		public guildOwner?: boolean
 	) {
 		this.avatar = d.member.avatar;
-		this.communication_disabled_until =
+		this.communicationDisabledUntil =
 			d.member.communication_disabled_until;
 		this.deaf = d.member.deaf;
-		this.joined_at = d.member.joined_at;
+		this.joinedAt = d.member.joined_at;
 		this.mute = d.member.mute;
 		this.nick = d.member.nick;
 		this.pending = d.member.pending;
-		this.premium_since = d.member.premium_since;
+		this.premiumSince = d.member.premium_since;
 		this.roles = d.member.roles;
+		this.user = d.user ? new User(d.user, this.client) : undefined
 		this.permission = BigInt(0);
 		this.roles.map((role) => {
 			const cachedRole = client.cache.roles.get(role);
 			if (cachedRole) {
 				this.permission =
-					// @ts-ignore
 					this.permission | BigInt(cachedRole.permissions);
 			}
 		});
+		this.createdAt = DiscordSnowflake.timestampFrom(d.user.id!)
 	}
 	async kick(reason?: string) {
 		let headers: undefined | HeadersInit;
@@ -46,7 +51,7 @@ export class GuildMember implements APIGuildMember {
 			headers.append("X-Audit-Log-Reason", reason);
 		}
 		await this.rest.request(
-			`/guilds/${this.d.guild_id!}/members/${this.user?.id}`,
+			endpoints.removeGuildBan(this.d.guild_id!, this.user?.id!),
 			"DELETE",
 			undefined,
 			headers
@@ -59,7 +64,7 @@ export class GuildMember implements APIGuildMember {
 			headers.append("X-Audit-Log-Reason", reason);
 		}
 		await this.rest.request(
-			`/guilds/${this.d.guild_id!}/bans/${this.user?.id}`,
+			endpoints.createGuildBan(this.d.guild_id!, this.user?.id!),
 			"PUT",
 			undefined,
 			headers
@@ -72,5 +77,65 @@ export class GuildMember implements APIGuildMember {
 			(this.permission & PermissionFlagsBits[permission]) ===
 			PermissionFlagsBits[permission]
 		);
+	}
+	async updateNickname(nickname: string, reason?: string) {
+		const headers = new Headers()
+		if (reason) headers.append("X-Audit-Log-Reason", reason)
+		await this.rest.request(
+			endpoints.modifyGuildMember(this.d.guild_id!, this.user?.id!),
+			"PATCH",
+			{ nick: nickname },
+			headers
+		)
+	}
+	async addRole(roleId: string, reason?: string) {
+		const headers = new Headers()
+		if (reason) headers.append("X-Audit-Log-Reason", reason)
+		await this.rest.request(
+			endpoints.addGuildMemberRole(this.d.guild_id!, this.user?.id!, roleId),
+			"PUT",
+			undefined,
+			headers
+		)
+	}
+	async removeRole(roleId: string, reason?: string) {
+		const headers = new Headers()
+		if (reason) headers.append("X-Audit-Log-Reason", reason)
+		await this.rest.request(
+			endpoints.removeGuildMemberRole(this.d.guild_id!, this.user?.id!,roleId),
+			"DELETE",
+			undefined,
+			headers
+		)
+	}
+	async moveToAnotherVoiceChannel(channelId: string, reason?: string) {
+		const headers = new Headers()
+		if (reason) headers.append("X-Audit-Log-Reason", reason)
+		await this.rest.request(
+			endpoints.modifyGuildMember(this.d.guild_id!, this.user?.id!),
+			"PATCH",
+			{ channel_id: channelId },
+			headers
+		)
+	}
+	async addTimeout(duration: string, reason?: string) {
+		const headers = new Headers()
+		if (reason) headers.append("X-Audit-Log-Reason", reason)
+		await this.rest.request(
+			endpoints.modifyGuildMember(this.d.guild_id!, this.user?.id!),
+			"PATCH",
+			{ communication_disabled_until: duration },
+			headers
+		)
+	}
+	async removeTimeout(reason?: string) {
+		const headers = new Headers()
+		if (reason) headers.append("X-Audit-Log-Reason", reason)
+		await this.rest.request(
+			endpoints.modifyGuildMember(this.d.guild_id!, this.user?.id!),
+			"PATCH",
+			{ communication_disabled_until: null },
+			headers
+		)
 	}
 }
