9bcffdeb98605e85c043ec8a247c4bb8c97824d2
Gurpal Singh <gillgurpal699@gmail.com>
2022-05-21 15:27:48 +0530

    create `MessageCreate` event handler

---
+++ src/events/MessageCreate.ts
@@ -0,0 +1,10 @@
+import { Base } from '../client/base.ts'
+import { Message } from '../structures/mod.ts'
+import { GatewayMessageCreateDispatchData } from '../types/mod.ts'
+export function MessageCreateGatewayEventHandler(data: GatewayMessageCreateDispatchData, client: Base) {
+    if (data.webhook_id) return
+    if (data.author.id !== client.user.id) {
+        const message = new Message(data, client)
+        client.emit('MessageCreate', message)
+    }
+}
