8a80120d96a7191a43455cefbb4f5dde82d85668
Gurpal Singh <gillgurpal699@gmail.com>
2022-05-21 15:29:17 +0530

    refactor `collectors.ts` to return an `Async Iterator`

--- src/structures/collectors/collector.ts
+++ src/structures/collectors/collector.ts
@@ -1,63 +1,35 @@
 import { Client } from "../../client/client.ts";
 import { ButtonInteraction } from "./classes/Button.ts";
 import { SelectMenuInteraction } from "./classes/SelectMenu.ts";
-export class ComponentCollector {
-	private target = new EventTarget();
+import { events } from '../../../deps.ts'
+type CollectorEvents = {
+	buttonInteraction: [interaction: ButtonInteraction]
+	selectMenuInteraction: [interaction: SelectMenuInteraction]
+}
+export class ComponentCollector extends events.EventEmitter<CollectorEvents> {
 	constructor(
 		private client: Client,
 		private channelId: string,
-		private disposeInterval?: number,
 	) {
+		super();
 		this.client.on("InteractionCreate", async (e: any) => {
 			if (
 				e.type == 3 &&
 				e.channelId == this.channelId &&
 				e.data.component_type == 2
 			) {
-				this.target.dispatchEvent(
-					new CustomEvent("buttonInteraction", {
-						detail: new ButtonInteraction(
-							this.client,
-							this.channelId,
-							e,
-						),
-					}),
-				);
+				super.emit("buttonInteraction",
+					new ButtonInteraction(this.client, this.channelId, e)
+				)
 			} else if (
 				e.type == 3 &&
 				e.channelId == this.channelId &&
 				e.data.component_type == 3
 			) {
-				this.target.dispatchEvent(
-					new CustomEvent("selectMenuInteraction", {
-						detail: new SelectMenuInteraction(
-							this.client,
-							this.channelId,
-							e,
-						),
-					}),
-				);
+				super.emit("selectMenuInteraction",
+					new SelectMenuInteraction(this.client, this.channelId, e)
+				)
 			}
 		});
-	}
-	waitForInteraction({
-		listener,
-		type,
-		once,
-	}: {
-		type: "buttonInteraction" | "selectMenuInteraction";
-		listener: (e: any) => any;
-		once?: boolean;
-	}) {
-		this.target.addEventListener(
-			type,
-			listener,
-			once ? { once: true } : {},
-		);
-		if (this.disposeInterval) {
-			setTimeout(() => {
-				this.target.removeEventListener(type, listener);
-			}, this.disposeInterval);
-		}
 	}
 }
