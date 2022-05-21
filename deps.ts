276bdcb5eb46c99b71d2c9b8327b091918f6db86
Gurpal Singh <gillgurpal699@gmail.com>
2022-05-21 15:25:10 +0530

    add new dependencies

--- deps.ts
+++ deps.ts
@@ -3,3 +3,7 @@ export { camelize } from "https://deno.land/x/camelize@2.0.0/mod.ts";
 export type { Camelize } from "https://deno.land/x/camelize@2.0.0/mod.ts";
 export * from "https://deno.land/x/discord_api_types@0.31.1/v10.ts";
 export * from "https://deno.land/x/collection@v1.0.1/mod.ts";
+export * from "https://deno.land/x/snowflakes@1.0.0/Discord.ts"
+export * from "https://deno.land/std@0.140.0/io/mod.ts"
+import * as events from 'https://deno.land/x/event@2.0.0/mod.ts'
+export { events }
