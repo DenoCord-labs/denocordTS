c8aedc850da2ad4a15e588521e973ed4b1d44620
Gurpal Singh <gillgurpal699@gmail.com>
2022-05-24 17:01:09 +0530

    fix `cache` types

--- src/types/cache.ts
+++ src/types/cache.ts
@@ -1,31 +1,23 @@
 import {
-	APIChannel,
-	APIEmoji,
-	APIGuild,
-	APIGuildMember,
-	APIRole,
-	APIUser,
-	Camelize,
-} from "./mod.ts";
-import {
-	DmChannel,
-	Guild,
-	GuildMember as Member,
-	TextChannel,
-	ThreadChannel,
-	User,
+  DmChannel,
+  Guild,
+  GuildEmoji,
+  GuildMember as Member,
+  Role as APIRole,
+  TextChannel,
+  ThreadChannel,
+  User,
 } from "../structures/mod.ts";
 import { Collection } from "../../deps.ts";
 export type Channel = DmChannel | TextChannel | ThreadChannel;
 
+export type Emoji = GuildEmoji;
 export type Role = APIRole;
-export type Emoji = APIEmoji;
-
 export type cacheFields = {
-	guilds: Collection<string, Guild>;
-	channels: Collection<string, DmChannel | TextChannel | ThreadChannel>;
-	users: Collection<string, User>;
-	emojis: Collection<string, Emoji>;
-	roles: Collection<string, Role>;
-	members: Collection<string, Member>;
+  guilds: Collection<string, Guild>;
+  channels: Collection<string, DmChannel | TextChannel | ThreadChannel>;
+  users: Collection<string, User>;
+  emojis: Collection<string, Emoji>;
+  roles: Collection<string, Role>;
+  members: Collection<string, Member>;
 };
