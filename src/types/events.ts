d407430aed0777c7e8782120fb30fb548584ab03
Gurpal Singh <gillgurpal699@gmail.com>
2022-05-24 17:01:29 +0530

    fix Gateway Events types

--- src/types/events.ts
+++ src/types/events.ts
@@ -1,67 +1,103 @@
-// deno-lint-ignore-file no-explicit-unknown
 import { ClientMessage } from "../structures/messages/mod.ts";
 import { BaseMessage } from "../structures/messages/Base.ts";
-import { APIMessage, APIMessageComponentButtonInteraction, APIMessageComponentSelectMenuInteraction, GatewayGuildDeleteDispatchData } from "./mod.ts";
-import { ApplicationCommandInteraction, Guild } from "../structures/mod.ts";
+import {
+  APIMessage,
+  APIMessageComponentButtonInteraction,
+  APIMessageComponentSelectMenuInteraction,
+  GatewayChannelPinsUpdateDispatchData,
+  GatewayGuildDeleteDispatchData,
+} from "./mod.ts";
+import {
+  ApplicationCommandInteraction,
+  DmChannel,
+  Guild,
+  GuildMember,
+  Role,
+  TextChannel,
+  ThreadChannel,
+} from "../structures/mod.ts";
 
 import { Camelize } from "../../deps.ts";
 
 export type Message = Camelize<APIMessage>;
 export type GatewayEvents = {
-	ChannelCreate: (e: unknown) => unknown;
-	ChannelDelete: (e: unknown) => unknown;
-	ChannelPinsUpdate: (e: unknown) => unknown;
-	ChannelUpdate: (e: unknown) => unknown;
-	GuildBanAdd: (e: unknown) => unknown;
-	GuildBanRemove: (e: unknown) => unknown;
-	GuildCreate: (e: Guild) => unknown;
-	GuildDelete: (e: Camelize<GatewayGuildDeleteDispatchData>) => unknown;
-	GuildEmojisUpdate: (e: unknown) => unknown;
-	GuildIntegrationsUpdate: (e: unknown) => unknown;
-	GuildMemberAdd: (e: unknown) => unknown;
-	GuildMemberRemove: (e: unknown) => unknown;
-	GuildMembersChunk: (e: unknown) => unknown;
-	GuildMemberUpdate: (e: unknown) => unknown;
-	GuildRoleCreate: (e: unknown) => unknown;
-	GuildRoleDelete: (e: unknown) => unknown;
-	GuildRoleUpdate: (e: unknown) => unknown;
-	GuildStickersUpdate: (e: unknown) => unknown;
-	GuildUpdate: (e: unknown) => unknown;
-	IntegrationCreate: (e: unknown) => unknown;
-	IntegrationDelete: (e: unknown) => unknown;
-	IntegrationUpdate: (e: unknown) => unknown;
-	InteractionCreate: (e: APIMessageComponentButtonInteraction | APIMessageComponentSelectMenuInteraction) => unknown;
-	InviteCreate: (e: unknown) => unknown;
-	InviteDelete: (e: unknown) => unknown;
-	MessageCreate: (e: ClientMessage | BaseMessage) => unknown;
-	MessageDelete: (e: Message) => unknown;
-	MessageDeleteBulk: (e: unknown) => unknown;
-	MessageReactionAdd: (e: unknown) => unknown;
-	MessageReactionRemove: (e: unknown) => unknown;
-	MessageReactionRemoveAll: (e: unknown) => unknown;
-	MessageReactionRemoveEmoji: (e: unknown) => unknown;
-	MessageUpdate: (e: unknown) => unknown;
-	PresenceUpdate: (e: unknown) => unknown;
-	StageInstanceCreate: (e: unknown) => unknown;
-	StageInstanceDelete: (e: unknown) => unknown;
-	StageInstanceUpdate: (e: unknown) => unknown;
-	Ready: (e: undefined) => unknown;
-	ThreadCreate: (e: unknown) => unknown;
-	ThreadDelete: (e: unknown) => unknown;
-	ThreadListSync: (e: unknown) => unknown;
-	ThreadMembersUpdate: (e: unknown) => unknown;
-	ThreadMemberUpdate: (e: unknown) => unknown;
-	ThreadUpdate: (e: unknown) => unknown;
-	TypingStart: (e: unknown) => unknown;
-	UserUpdate: (e: unknown) => unknown;
-	VoiceServerUpdate: (e: unknown) => unknown;
-	VoiceStateUpdate: (e: unknown) => unknown;
-	WebhooksUpdate: (e: unknown) => unknown;
-	GuildScheduledEventCreate: (e: unknown) => unknown;
-	GuildScheduledEventUpdate: (e: unknown) => unknown;
-	GuildScheduledEventDelete: (e: unknown) => unknown;
-	GuildScheduledEventUserAdd: (e: unknown) => unknown;
-	GuildScheduledEventUserRemove: (e: unknown) => unknown;
-	CommandInteraction: (e: ApplicationCommandInteraction) => unknown;
-	Error: (e: unknown) => unknown;
+  ChannelCreate: (
+    e: DmChannel | TextChannel | ThreadChannel | undefined,
+  ) => unknown;
+  ChannelDelete: (
+    e: DmChannel | TextChannel | ThreadChannel | undefined,
+  ) => unknown;
+  ChannelPinsUpdate: (
+    e: Camelize<GatewayChannelPinsUpdateDispatchData>,
+  ) => unknown;
+  ChannelUpdate: (
+    e: DmChannel | TextChannel | ThreadChannel | undefined,
+  ) => unknown;
+  GuildBanAdd: (e: unknown) => unknown;
+  GuildBanRemove: (e: unknown) => unknown;
+  GuildCreate: (e: Guild) => unknown;
+  GuildDelete: (e: Camelize<GatewayGuildDeleteDispatchData>) => unknown;
+  GuildEmojisUpdate: (e: unknown) => unknown;
+  GuildIntegrationsUpdate: (e: unknown) => unknown;
+  GuildMemberAdd: (e: unknown) => unknown;
+  GuildMemberRemove: (e: unknown) => unknown;
+  GuildMembersChunk: (e: unknown) => unknown;
+  GuildMemberUpdate: (e: GuildMember) => unknown;
+  GuildRoleCreate: (e: Role) => unknown;
+  GuildRoleDelete: (
+    e: Omit<
+      Role,
+      | "setName"
+      | "setColor"
+      | "setPermissions"
+      | "setPosition"
+      | "setHoist"
+      | "setMentionable"
+      | "setUnicodeEmoji"
+    >,
+  ) => unknown;
+  GuildRoleUpdate: (e: Role) => unknown;
+  GuildStickersUpdate: (e: unknown) => unknown;
+  GuildUpdate: (e: unknown) => unknown;
+  IntegrationCreate: (e: unknown) => unknown;
+  IntegrationDelete: (e: unknown) => unknown;
+  IntegrationUpdate: (e: unknown) => unknown;
+  InteractionCreate: (
+    e:
+      | APIMessageComponentButtonInteraction
+      | APIMessageComponentSelectMenuInteraction,
+  ) => unknown;
+  InviteCreate: (e: unknown) => unknown;
+  InviteDelete: (e: unknown) => unknown;
+  MessageCreate: (e: ClientMessage | BaseMessage) => unknown;
+  MessageDelete: (e: Message) => unknown;
+  MessageDeleteBulk: (e: unknown) => unknown;
+  MessageReactionAdd: (e: unknown) => unknown;
+  MessageReactionRemove: (e: unknown) => unknown;
+  MessageReactionRemoveAll: (e: unknown) => unknown;
+  MessageReactionRemoveEmoji: (e: unknown) => unknown;
+  MessageUpdate: (e: unknown) => unknown;
+  PresenceUpdate: (e: unknown) => unknown;
+  StageInstanceCreate: (e: unknown) => unknown;
+  StageInstanceDelete: (e: unknown) => unknown;
+  StageInstanceUpdate: (e: unknown) => unknown;
+  Ready: (e: undefined) => unknown;
+  ThreadCreate: (e: ThreadChannel) => unknown;
+  ThreadDelete: (e: unknown) => unknown;
+  ThreadListSync: (e: unknown) => unknown;
+  ThreadMembersUpdate: (e: unknown) => unknown;
+  ThreadMemberUpdate: (e: unknown) => unknown;
+  ThreadUpdate: (e: ThreadChannel) => unknown;
+  TypingStart: (e: unknown) => unknown;
+  UserUpdate: (e: unknown) => unknown;
+  VoiceServerUpdate: (e: unknown) => unknown;
+  VoiceStateUpdate: (e: unknown) => unknown;
+  WebhooksUpdate: (e: unknown) => unknown;
+  GuildScheduledEventCreate: (e: unknown) => unknown;
+  GuildScheduledEventUpdate: (e: unknown) => unknown;
+  GuildScheduledEventDelete: (e: unknown) => unknown;
+  GuildScheduledEventUserAdd: (e: unknown) => unknown;
+  GuildScheduledEventUserRemove: (e: unknown) => unknown;
+  CommandInteraction: (e: ApplicationCommandInteraction) => unknown;
+  Error: (e: unknown) => unknown;
 };
