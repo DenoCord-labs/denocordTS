529ea04e258116fe34036ad83445800741b2db4b
Gurpal Singh <gillgurpal699@gmail.com>
2022-05-24 16:58:31 +0530

    handle more gateway events

--- src/client/base.ts
+++ src/client/base.ts
@@ -1,237 +1,297 @@
 import { CacheObject } from "../cache/mod.ts";
 import {
-	APIChannel,
-	APIEmoji,
-	APIRole,
-	APIUser,
-	GatewayDispatchEvents,
-	GatewayIdentifyData,
-	GatewayIntentBits,
-	GatewayOpcodes,
-	ChannelType
+  APIChannel,
+  APIEmoji,
+  APIRole,
+  APIUser,
+  ChannelType,
+  GatewayChannelPinsUpdateDispatchData,
+  GatewayDispatchEvents,
+  GatewayIdentifyData,
+  GatewayIntentBits,
+  GatewayOpcodes,
 } from "../types/mod.ts";
-import { Camelize, camelize, EventEmitter, parse, stringify } from "../../deps.ts";
 import {
-	APIMessage,
-	ClientOptions,
-	ClientUser,
-	GatewayEvents,
-	OPCodes,
-	APIGuildMember
+  Camelize,
+  camelize,
+  EventEmitter,
+  parse,
+  stringify,
+} from "../../deps.ts";
+import {
+  APIGuildMember,
+  APIMessage,
+  ClientOptions,
+  ClientUser,
+  GatewayEvents,
+  OPCodes,
 } from "../types/mod.ts";
 import { GatewayUrl } from "../constants/mod.ts";
-import { ApplicationCommandInteraction, TextChannel, DmChannel, ThreadChannel } from "../structures/mod.ts";
+import {
+  ApplicationCommandInteraction,
+  DmChannel,
+  Guild,
+  GuildEmoji,
+  GuildMember,
+  Role,
+  TextChannel,
+  ThreadChannel,
+  User,
+} from "../structures/mod.ts";
 import { CloseEventHandler } from "../handler/mod.ts";
 import {
-	GatewayReadyEventHandler,
-	MessageCreateGatewayEventHandler,
+  GatewayReadyEventHandler,
+  MessageCreateGatewayEventHandler,
 } from "../events/mod.ts";
 
 export class Base extends EventEmitter<GatewayEvents> {
-	public cache;
-	private heartbeatInterval = 41250;
-	protected websocket: WebSocket;
-	public user = {} as ClientUser;
-	token = "";
-	public readonly uptime = new Date().getTime();
-	protected start: number = Date.now();
-	protected options;
-	protected cachedInstance = new CacheObject(this);
-	ping = -1;
-	constructor(options: ClientOptions) {
-		super();
-		this.options = options;
-		this.token = options.token;
-		this.websocket = new WebSocket(GatewayUrl);
-		this.cache = this.cachedInstance.cache;
-		const payload: GatewayIdentifyData = {
-			token: options.token,
-			intents: options.intents.reduce(
-				(bits, next) => (bits |= GatewayIntentBits[next]),
-				0,
-			),
-			properties: {
-				$browser: "denocordts",
-				$device: "denocordts",
-				$os: Deno.build.os,
-			},
-		};
-		this.websocket.onopen = async () => {
-			await this.websocket.send(JSON.stringify({
-				op: GatewayOpcodes.Identify,
-				d: {
-					...payload,
-				},
-			}));
-		};
-		this.websocket.onerror = async (e) => {
-			await this.emit("Error", e);
-		};
-		this.websocket.onmessage = async (e) => {
-			const { d, op, t } = JSON.parse(e.data);
+  public cache;
+  private heartbeatInterval = 41250;
+  protected websocket: WebSocket;
+  public user = {} as ClientUser;
+  token = "";
+  public readonly uptime = new Date().getTime();
+  protected start: number = Date.now();
+  protected options;
+  protected cachedInstance = new CacheObject(this);
+  ping = -1;
+  constructor(options: ClientOptions) {
+    super();
+    this.options = options;
+    this.token = options.token;
+    this.websocket = new WebSocket(GatewayUrl);
+    this.cache = this.cachedInstance.cache;
+    const payload: GatewayIdentifyData = {
+      token: options.token,
+      intents: options.intents.reduce(
+        (bits, next) => (bits |= GatewayIntentBits[next]),
+        0,
+      ),
+      properties: {
+        $browser: "denocordts",
+        $device: "denocordts",
+        $os: Deno.build.os,
+      },
+    };
+    this.websocket.onopen = async () => {
+      await this.websocket.send(JSON.stringify({
+        op: GatewayOpcodes.Identify,
+        d: {
+          ...payload,
+        },
+      }));
+    };
+    this.websocket.onerror = async (e) => {
+      await this.emit("Error", e);
+    };
+    this.websocket.onmessage = async (e) => {
+      const { d, op, t } = JSON.parse(e.data);
 
-			switch (op) {
-				case OPCodes.HELLO: {
-					this.heartbeatInterval = d.heartbeat_interval;
-					this.ping = Date.now() - this.start;
-					this.sendHeartBeat();
-					break;
-				}
-				case OPCodes.HEARTBEAT_ACK: {
-					this.ping = Date.now() - this.start;
-				}
-			}
-			switch (t) {
-				case GatewayDispatchEvents.MessageCreate: {
-					MessageCreateGatewayEventHandler(d, this);
-					break;
-				}
-				case GatewayDispatchEvents.Ready: {
-					GatewayReadyEventHandler(d, this);
-					break;
-				}
-				case GatewayDispatchEvents.GuildCreate: {
-					await this.cachedInstance.addGuildToCache(d.id, d);
-					await this.addRolesToCache(d.roles);
-					await this.addEmojisToCache(d.emojis)
-					await this.addChannelsToCache(d.channels)
-					await this.addUsersToCache(d.members.map((member: APIGuildMember) => member.user));
-					const length = this.cache.guilds.array().length;
-					const guildsLength = parse(stringify(this.user)).guilds.length;
-					if (length === guildsLength) {
-						return this.emit("Ready", undefined);
-					}
-					if (length > guildsLength) {
-						this.emit("GuildCreate", this.cache.guilds.get(d.id)!);
-					}
-					break;
-				}
-				case GatewayDispatchEvents.InteractionCreate: {
-					console.log(d.type)
-					switch (d.type) {
-						case 3: {
-							this.emit(
-								"InteractionCreate",
-								d
-							);
-							break
-						}
-						case 1: break
-						default: {
-							this.emit("CommandInteraction", new ApplicationCommandInteraction(d, this.options.token, this));
+      switch (op) {
+        case OPCodes.HELLO: {
+          this.heartbeatInterval = d.heartbeat_interval;
+          this.ping = Date.now() - this.start;
+          this.sendHeartBeat();
+          break;
+        }
+        case OPCodes.HEARTBEAT_ACK: {
+          this.ping = Date.now() - this.start;
+        }
+      }
+      switch (t) {
+        case GatewayDispatchEvents.MessageCreate: {
+          MessageCreateGatewayEventHandler(d, this);
+          break;
+        }
+        case GatewayDispatchEvents.Ready: {
+          GatewayReadyEventHandler(d, this);
+          break;
+        }
+        case GatewayDispatchEvents.GuildCreate: {
+          await this.cachedInstance.addGuildToCache(d.id, d);
+          await this.addRolesToCache(d.roles, this.cache.guilds.get(d.id)!);
+          await this.addEmojisToCache(d.emojis, d.id);
+          this.addChannelsToCache(d.channels);
+          await this.addUsersToCache(
+            d.members.map((member: APIGuildMember) => member.user),
+          );
+          d.members.forEach((member: APIGuildMember) =>
+            this.cache.members.set(
+              member.user!.id,
+              new GuildMember({ member }, this, d.owner_id === member.user!.id),
+            )
+          );
+          const length = this.cache.guilds.array().length;
+          const guildsLength = parse(stringify(this.user)).guilds.length;
+          if (length === guildsLength) {
+            return this.emit("Ready", undefined);
+          }
+          if (length > guildsLength) {
+            this.emit("GuildCreate", this.cache.guilds.get(d.id)!);
+          }
+          break;
+        }
+        case GatewayDispatchEvents.InteractionCreate: {
+          Deno.writeTextFileSync("interaction.json", JSON.stringify(d));
+          console.log("interaction");
+          switch (d.type) {
+            case 3: {
+              this.emit(
+                "InteractionCreate",
+                d,
+              );
+              break;
+            }
+            case 1:
+              break;
+            default: {
+              this.emit(
+                "CommandInteraction",
+                new ApplicationCommandInteraction(d, this.options.token, this),
+              );
+              break;
+            }
+          }
+        }
+        case GatewayDispatchEvents.GuildMemberUpdate: {
+          const owner =
+            this.cache.guilds.get(d.guild_id)?.ownerId === d.user.id;
+          const member = new GuildMember(d, this, owner);
+          this.emit("GuildMemberUpdate", member);
+          break;
+        }
+        case GatewayDispatchEvents.GuildRoleCreate: {
+          if (d.role) {
+            const role = new Role(d.role, this.cache.guilds.get(d.guild_id)!);
+            this.cache.roles.set(d.role.id, role);
+            this.emit("GuildRoleCreate", role);
+          }
+          break;
+        }
+        case GatewayDispatchEvents.GuildRoleDelete: {
+          const role = this.cache.roles.get(d.role_id)!;
+          this.emit("GuildRoleDelete", role);
+          this.cache.roles.delete(d.role_id);
+          break;
+        }
+        case GatewayDispatchEvents.GuildRoleUpdate: {
+          if (d.role) {
+            const role = new Role(d, this.cache.guilds.get(d.guild_id)!);
+            this.cache.roles.set(d.role.id, role);
+            this.emit("GuildRoleUpdate", role);
+          }
+          break;
+        }
+        case GatewayDispatchEvents.MessageDelete: {
+          this.emit(
+            "MessageDelete",
+            camelize(d) as Camelize<APIMessage>,
+          );
+          break;
+        }
+        case GatewayDispatchEvents.ChannelCreate: {
+          const channel = this.addChannelsToCache([d]);
+          this.emit("ChannelCreate", channel);
+          break;
+        }
+        case GatewayDispatchEvents.ChannelUpdate: {
+          const channel = this.addChannelsToCache([d]);
+          this.emit("ChannelUpdate", channel);
+        }
+        case GatewayDispatchEvents.ChannelDelete: {
+          const channel = this.cache.channels.get(d.id);
+          this.cache.channels.delete(d.id);
+          this.emit("ChannelDelete", channel);
+        }
+        case GatewayDispatchEvents.ChannelPinsUpdate: {
+          this.emit(
+            "ChannelPinsUpdate",
+            camelize(d) as Camelize<GatewayChannelPinsUpdateDispatchData>,
+          );
+        }
+        case GatewayDispatchEvents.ThreadCreate: {
+          const thread = new ThreadChannel(d, this);
+          this.emit("ThreadCreate", thread);
+        }
+        case GatewayDispatchEvents.ThreadUpdate: {
+          const thread = this.cache.channels.get(d.channel_id) as ThreadChannel;
+          this.cache.channels.set(d.channel_id, thread);
+          this.emit("ThreadUpdate", thread);
+        }
+      }
+    };
+    this.websocket.onclose = (e) => {
+      new CloseEventHandler(e.code);
+    };
+  }
+  private sendHeartBeat() {
+    setInterval(() => {
+      this.start = Date.now();
+      this.websocket.send(JSON.stringify({
+        op: OPCodes.HEARTBEAT,
+        d: null,
+      }));
+    }, this.heartbeatInterval);
+  }
 
-							break;
-						}
-					}
-				}
-				case GatewayDispatchEvents.GuildMemberUpdate: {
-					break;
-				}
-				case GatewayDispatchEvents.GuildRoleCreate: {
-					if (d.role) {
-						this.cache.roles.set(
-							d.role.id,
-							camelize(d.role) as any,
-						);
-					}
-					break;
-				}
-				case GatewayDispatchEvents.GuildRoleDelete: {
-					this.cache.roles.delete(d.role_id);
-					break;
-				}
-				case GatewayDispatchEvents.GuildRoleUpdate: {
-					d.role &&
-						d.role &&
-						this.cache.roles.set(
-							d.role.id,
-							camelize(d.role) as any,
-						);
-					break;
-				}
-				case GatewayDispatchEvents.MessageDelete: {
-					this.emit(
-						"MessageDelete",
-						camelize(d) as Camelize<APIMessage>,
-					);
-					break;
-				}
-				case GatewayDispatchEvents.ChannelCreate: {
-					this.addChannelsToCache([d])
-					break;
-				}
-			}
-		};
-		this.websocket.onclose = (e) => {
-			new CloseEventHandler(e.code);
-		};
-	}
-	private sendHeartBeat() {
-		setInterval(() => {
-			this.start = Date.now();
-			this.websocket.send(JSON.stringify({
-				op: OPCodes.HEARTBEAT,
-				d: null,
-			}));
-		}, this.heartbeatInterval);
-	}
-
-	private async addRolesToCache(roles: APIRole[]) {
-		await Promise.all(
-			roles.map(async (role) => {
-				this.cachedInstance.addRoleToCache(role.id, role as any);
-			}),
-		);
-	}
-	private async addEmojisToCache(emojis: APIEmoji[]) {
-		await Promise.all(
-			emojis.map((emoji) => {
-				this.cachedInstance.addEmojiToCache(emoji.id!, emoji as any);
-			}),
-		);
-	}
-	private async addUsersToCache(users: APIUser[]) {
-		await Promise.all(
-			users.map((user) => {
-				this.cachedInstance.addUserToCache(user.id, user as any);
-			}),
-		);
-	}
-	private async addChannelsToCache(channels: APIChannel[]) {
-		Deno.writeTextFileSync("channelsGateway.json", JSON.stringify(channels));
-		for (let index = channels.length - 1; index >= 0; index--) {
-			const channelPayload = channels[index]
-			switch (channelPayload.type) {
-				case ChannelType.DM: {
-					await this.cache.channels.set(
-						channelPayload.id as string,
-						new DmChannel(channelPayload, this),
-					);
-					break;
-				}
-				case ChannelType.GuildText: {
-					await this.cache.channels.set(
-						channelPayload.id as string,
-						new TextChannel(channelPayload, this),
-					);
-					break;
-				}
-				case ChannelType.GuildPrivateThread: {
-					await this.cache.channels.set(
-						channelPayload.id as string,
-						new ThreadChannel(channelPayload, this),
-					);
-					break;
-				}
-				case ChannelType.GuildPublicThread: {
-					await this.cache.channels.set(
-						channelPayload.id as string,
-						new ThreadChannel(channelPayload, this),
-					);
-					break;
-				}
-			}
-
-		}
-	}
+  private async addRolesToCache(roles: APIRole[], guild: Guild) {
+    await Promise.all(
+      roles.map(async (role) => {
+        this.cachedInstance.addRoleToCache(role.id, new Role(role, guild));
+      }),
+    );
+  }
+  private async addEmojisToCache(emojis: APIEmoji[], guildId: string) {
+    await Promise.all(
+      emojis.map((emoji) => {
+        this.cachedInstance.addEmojiToCache(
+          emoji.id!,
+          new GuildEmoji(emoji as any, this, guildId),
+        );
+      }),
+    );
+  }
+  private async addUsersToCache(users: APIUser[]) {
+    await Promise.all(
+      users.map((user) => {
+        this.cachedInstance.addUserToCache(user.id, new User(user, this));
+      }),
+    );
+  }
+  private addChannelsToCache(channels: APIChannel[]) {
+    for (let index = channels.length - 1; index >= 0; index--) {
+      const channelPayload = channels[index];
+      switch (channelPayload.type) {
+        case ChannelType.DM: {
+          this.cache.channels.set(
+            channelPayload.id as string,
+            new DmChannel(channelPayload, this),
+          );
+          break;
+        }
+        case ChannelType.GuildText: {
+          this.cache.channels.set(
+            channelPayload.id as string,
+            new TextChannel(channelPayload, this),
+          );
+          break;
+        }
+        case ChannelType.GuildPrivateThread: {
+          this.cache.channels.set(
+            channelPayload.id as string,
+            new ThreadChannel(channelPayload, this),
+          );
+          break;
+        }
+        case ChannelType.GuildPublicThread: {
+          this.cache.channels.set(
+            channelPayload.id as string,
+            new ThreadChannel(channelPayload, this),
+          );
+          break;
+        }
+      }
+      return this.cache.channels.get(channelPayload.id);
+    }
+  }
 }
