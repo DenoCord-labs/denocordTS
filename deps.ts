77d21fb4c2a38fb557fde62ba2008e9ecc7d9208
Gurpal Singh <gillgurpal699@gmail.com>
2022-06-17 13:13:53 +0530

    update dependencies

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
 export * from "https://deno.land/x/eventemitter@1.2.1/mod.ts";
 export { camelize } from "https://deno.land/x/camelize@2.0.0/mod.ts";
 export type { Camelize } from "https://deno.land/x/camelize@2.0.0/mod.ts";
-export * from "https://deno.land/x/discord_api_types@0.31.1/v10.ts";
+export * from "https://deno.land/x/discord_api_types@0.34.0/v10.ts";
 export * from "https://deno.land/x/collection@v1.0.1/mod.ts";
 export * from "https://deno.land/x/snowflakes@1.0.0/Discord.ts";
-export * from "https://deno.land/std@0.140.0/io/mod.ts";
+export * from "https://deno.land/std@0.144.0/fmt/colors.ts";
 export { parse, stringify } from "https://esm.sh/flatted@3.2.5";
-export * as Colors from "https://deno.land/std@0.143.0/fmt/colors.ts"
+export * as Colors from "https://deno.land/std@0.144.0/fmt/colors.ts"
