8e17ab2020a49993b49b76aad86362eacaf537a3
Gurpal Singh <gillgurpal699@gmail.com>
2022-08-19 22:46:17 +0530

    chore: update package version

--- deps.ts
+++ deps.ts
@@ -1,9 +1,10 @@
-export * from "https://deno.land/x/eventemitter@1.2.1/mod.ts";
+export * from "https://deno.land/std@0.144.0/fmt/colors.ts";
+export * as Colors from "https://deno.land/std@0.144.0/fmt/colors.ts";
 export { camelize } from "https://deno.land/x/camelize@2.0.0/mod.ts";
 export type { Camelize } from "https://deno.land/x/camelize@2.0.0/mod.ts";
-export * from "https://deno.land/x/discord_api_types@0.34.0/v10.ts";
 export * from "https://deno.land/x/collection@v1.0.1/mod.ts";
+export * from "https://deno.land/x/discord_api_types@0.37.3/v10.ts";
+export * from "https://deno.land/x/eventemitter@1.2.1/mod.ts";
 export * from "https://deno.land/x/snowflakes@1.0.0/Discord.ts";
-export * from "https://deno.land/std@0.144.0/fmt/colors.ts";
 export { parse, stringify } from "https://esm.sh/flatted@3.2.5";
-export * as Colors from "https://deno.land/std@0.144.0/fmt/colors.ts";
+
